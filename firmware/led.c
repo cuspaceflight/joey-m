c6b1d77f999cc5643bc4436451228bb9b8fa7adf
Jon Sowman <jon@hexoc.com>
2012-10-26 15:52:20 +0100

    Added some comments to the LED files

--- firmware/led.c
+++ firmware/led.c
@@ -8,11 +8,18 @@
 #include <avr/io.h>
 #include "led.h"
 
+/**
+ * Initialise the LEDs by setting them as outputs and turning
+ * them off.
+ */
 void led_init()
 {   
     LED_DDR |= _BV(LED_RED) | _BV(LED_GREEN);
 }
 
+/**
+ * Set the given LED to the given state.
+ */
 void led_set(int led, int status) {
     if( status & 0x01 )
         LED_PORT |= _BV(led & 0x01);
