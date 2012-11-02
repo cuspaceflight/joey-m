ea9c6cbfbf4d31aa364f4f2746ab0a723def9bf4
Jon Sowman <jon@hexoc.com>
2012-11-02 15:40:08 +0000

    Empty files for the temperature stuff

---
+++ firmware/temperature.c
@@ -0,0 +1,9 @@
+/**
+ * JOEY-M by CU Spaceflight
+ *
+ * This file is part of the JOEY-M project by Cambridge University Spaceflight.
+ *
+ * Jon Sowman 2012
+ */
+
+#include "temperature.h"
---
+++ firmware/temperature.h
@@ -0,0 +1,15 @@
+/**
+ * JOEY-M by CU Spaceflight
+ *
+ * This file is part of the JOEY-M project by Cambridge University Spaceflight.
+ *
+ * Jon Sowman 2012
+ */
+
+#ifndef __TEMPERATURE_H__
+#define __TEMPERATURE_H__
+
+void temperature_init(void);
+float temperature_read(void);
+
+#endif /* __TEMPERATURE_H__ */
