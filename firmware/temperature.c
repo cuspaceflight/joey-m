abc813f33cab7f2ea4cdc083cdd4103f27ff0944
Jon Sowman <jon@hexoc.com>
2012-11-09 15:00:56 +0000

    temp stuff

--- firmware/temperature.c
+++ firmware/temperature.c
@@ -7,3 +7,48 @@
  */
 
 #include "temperature.h"
+
+/**
+ * Set up the TMP100 temperature sensor.
+ */
+void temperature_init()
+{
+    // Set up the prescaler to give a clock frequency of 400kHz
+    TWSR |= _BV(TWPS1);
+    TWBR = 1;
+
+    // Enable the I2C interface and enable interrupts
+    TWCR |= _BV(TWEN) | _BV(TWIE);
+}
+
+/**
+ * Return the current temperature.
+ */
+float temperature_read()
+{
+    // Become a master on the bus and transmit a start bit
+    TWCR |= _BV(TWINT) | _BV(TWSTA);
+
+    // Check that the start was successfully sent
+}
+
+/**
+ * Interrupt for when the I2C interface fires an interrupt for whatever reason.
+ */
+ISR(TWI_vect)
+{
+    // What we do depends on the value of the status register
+    switch(TWSR)
+    {
+        case TW_START_SENT:
+            // Transmit SLA+W (R/W bit (LSB) is 0 for W)
+            TWDR = TMP100_ADDR << 1;
+            TWCR |= _BV(TWINT);
+            break;
+
+        default:
+            break;
+    }
+    return;
+}
+
--- firmware/temperature.h
+++ firmware/temperature.h
@@ -9,6 +9,17 @@
 #ifndef __TEMPERATURE_H__
 #define __TEMPERATURE_H__
 
+#define TMP100_ADDR         1001011
+
+// Status codes for the onewire interface
+#define     TW_START_SENT           0x08
+#define     TW_RPT_START_SENT       0x10
+#define     TW_SLAW_ACK             0x18
+#define     TW_SLAW_NACK            0x20
+#define     TW_DATA_ACK             0x28
+#define     TW_DATA_NACK            0x30
+#define     TW_SLAW_DATA_ARBLOST    0x38
+
 void temperature_init(void);
 float temperature_read(void);
 
